68b54212fcc1e60b7a80ad30e97b90d80ceb80c0
Ali Adnan <144818736+AliAdnan2107@users.noreply.github.com>
2025-02-06 11:00:34 -0400

    Update init.py

---
+++ Object Oriented Programming/init.py
@@ -0,0 +1,13 @@
+class Pet:
+    def __init__(self, age, gender, breed, animal):
+        self.animal=animal
+        self.breed=breed
+        self.age=age
+        self.gender=gender
+
+
+p1=Pet("6 Years old","Male","Serbian","Cat")
+p2=Pet("2 Years old","Female","Golden Retriever","Dog")
+
+print ("The age of pet 1 is", p1.age)
+print ("The Breed of pet 2 is", p2.breed)
