14f683abec03df42da750067a1b48ce0a3c451ae
Ali Adnan <144818736+AliAdnan2107@users.noreply.github.com>
2025-02-24 11:24:44 -0400

    w

--- Object Oriented Programming/Final Project (TEXT BASED GAME)/main.py
+++ Object Oriented Programming/Final Project (TEXT BASED GAME)/main.py
@@ -11,6 +11,7 @@ everything...
 #Imports
 import time
 from characters import *
+from initdialogue import *
 
 
 
@@ -41,32 +42,8 @@ def Sleep():
     time.sleep(2)
 
 def Game():
-    print("")
-    print ("UNKNOWN: You're awake")
-    Sleep()
-    print ("YOU: I am?")
-    Sleep()
-    print ("UNKNOWN: God I don't think you remember a thing")
-    Sleep()
-    print ("YOU: What happened?")
-    Sleep()
-    print ("UNKNOWN: He got to you...")
-    Sleep()
-    print ("YOU: Who Did?")
-    Sleep()
-    print ("UNKNOWN: The Master. He Betrayed you")
-    Sleep()
-    print ("YOU: Oh, He stole the sword aswell")
-    Sleep()
-    print ("UNKNOWN: THE Sword?")
-    Sleep()
-    print ("YOU: THE Sword.")
-    Sleep()
-    print ("UNKNOWN: What's your name partner?")
-    username=input("Type in your name: ")
-    print (f"{username}: The Name's {username}, and you are?")
-    print ("ALLOY: I'm alloy, I found you lying in the woods after you we're betrayed, Thought you were dead")
-
+   
+    
 
 
 #Main -----------------------------------------------------
