272ed25dc6d3c20e2a1113d6fbc30b2ac35a0d65
Ali Adnan <144818736+AliAdnan2107@users.noreply.github.com>
2025-02-26 10:26:49 -0400

    Update characters.py

--- Object Oriented Programming/Final Project (TEXT BASED GAME)/characters.py
+++ Object Oriented Programming/Final Project (TEXT BASED GAME)/characters.py
@@ -3,6 +3,8 @@
 #February 21st 2025
 #PROJECT TEXT BASED GAME - CHARACTERS
 
+from initdialogue import *
+
 
 class Characters:
     def __init__ (self, Name, Health, Strength,):
@@ -15,7 +17,20 @@ class Foe(Characters):
         Characters.__init__(self, Name, Health, Strength)
         self.EvilnessMeter=EvilnessMeter
 
+class Friend(Characters):
+    def __init__(self, Name, Health, Strength, FriendshipMeter):
+        Characters.__init__(self, Name, Health, Strength)
+        self.FriendshipMeter=FriendshipMeter
+
 class MainCharacters(Characters):
     def __init__(self, Name, Health, Strength, XP ):
         Characters.__init__(self, Name, Health, Strength)
         self.XP=XP
+
+
+
+def BuildPick():
+    print ("Select your initial build: ")
+    print ("1 - 150 Health, 60 Strength, 20XP")
+    print ("2 - 100 Health, 80 Strength, 40XP")
+    print ("3 - 80 Health, 120 Strength, 30XP")
