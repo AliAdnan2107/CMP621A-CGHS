4536436afa30979c1ceb076d8c1f6817803223ac
Ali Adnan <144818736+AliAdnan2107@users.noreply.github.com>
2025-04-07 09:13:52 -0300

    a

---
+++ Web Dev/JS/script.js
@@ -0,0 +1,17 @@
+console.log("What","Do","Commas","Do?");
+console.log("Does",       "Space", "Make     ", "A", "Difference?");
+console.log("What about " + "addition?")
+console.log("And how about our beautiful integers like", 4)
+alert("Hey man you wanna go ahead and hit the ok right there so you can see what happens?")
+
+function ButtonAlert () {
+    alert("You clicked the button! \n How did you get it first try?")
+}
+
+function DocWrite() {
+    document.write("This is a document write test")
+}
+
+
+document.write("This is a document write test <br>")
+document.write("Hello World!")
